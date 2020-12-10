aef49925748a8b6620f0ef227df51c3d43fb3adc
Davin Tryon <davintryon@gmail.com>
2020-12-10 13:11:56 +0000

    add progress

---
+++ day10/day10.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+
+const cache = {};
+const calcHash = list => list.reduce((acc, next) => acc + next, '');
+
+const countBranches = (list) => {
+	if (cache[calcHash(list)]) {
+		return cache[calcHash(list)];
+	}
+
+	const [ first, second = 10000, third = 10000, fourth = 10000, ...tail ] = list;
+	const diffOne = second - first;
+	const diffTwo = third - second;
+	const diffThree = fourth - third;
+
+	const jumpOne = diffOne;
+	const jumpTwo = diffOne + diffTwo;
+	const jumpThree = diffOne + diffTwo + diffThree;
+
+	if (second === 10000) {
+		return 1;
+	}
+
+	const jumpOneList = [second, third, fourth, ...tail];
+	const jumpTwoList = [third, fourth, ...tail];
+	const jumpThreeList = [fourth, ...tail];
+
+	if (jumpThree <= 3) {
+		const ar = cache[calcHash(jumpOneList)] = countBranches(jumpOneList);
+		const br = cache[calcHash(jumpTwoList)] = countBranches(jumpTwoList);
+		const cr = cache[calcHash(jumpThreeList)] = countBranches(jumpThreeList);
+		return ar + br + cr;
+	}
+
+	if (jumpTwo <= 3) {
+		const dr = cache[calcHash(jumpOneList)] = countBranches(jumpOneList);
+		const er = cache[calcHash(jumpTwoList)] = countBranches(jumpTwoList);
+		return dr + er;
+	}
+
+	const fr = cache[calcHash(jumpOneList)] = countBranches(jumpOneList);
+	return fr;
+}
+
+const main = () => {
+	const numbers = fs.readFileSync('input.txt', {encoding:'utf8', flag:'r'});
+	const list = numbers.split(/\n/).filter(n => n).map((r, i) => {
+		return parseInt(r, 10);
+	});
+
+	list.sort((a, b) => a - b);
+
+	const builtIn = list[list.length-1]+3;
+
+	list.push(builtIn);
+	list.unshift(0);
+
+	let prev = 0;
+	let result = {};
+
+	for(let i = 0; i < list.length; i++) {
+		const curr = list[i];
+		if (result[`${curr-prev}`]) {
+			result[`${curr-prev}`]++;
+		} else {
+			result[`${curr-prev}`] = 1;
+		}
+		prev = curr;
+	}
+
+	const stepOne = result['1'] * result['3'];
+
+	console.log(result);
+	console.log(stepOne);
+	
+	const stepTwo = countBranches(list);
+
+	console.log(stepTwo);
+
+}
+
+main();
